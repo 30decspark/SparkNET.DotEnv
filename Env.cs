9ed1378090044bc2778c17efc78ab31d27f5f140
30decspark <30decspark@gmail.com>
2025-03-27 17:43:50 +0700

    initial 1.0.0

---
+++ Env.cs
@@ -0,0 +1,38 @@
+﻿namespace SparkNET.DotEnv
+{
+    public static class Env
+    {
+        private static Dictionary<string, string> _data = [];
+
+        public static void Load()
+        { 
+            string path = ".env";
+            if (!File.Exists(path))
+            {
+                throw new Exception("The .env file does not exist!");
+            }
+
+            _data = [];
+            var lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line.Trim()) || line.Trim().StartsWith('#'))
+                {
+                    continue;
+                }
+
+                var kvp = line.Split('=', 2);
+                if (kvp.Length == 2)
+                {
+                    var key = kvp[0].Trim();
+                    var value = kvp[1].Trim();
+
+                    if (!string.IsNullOrWhiteSpace(key) && !string.IsNullOrWhiteSpace(value))
+                    {
+                        _data[key] = value;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+﻿## Usage
+
+To use **SparkNET.DotEnv** for load .env file.
+
+### Example Code:
+
+```csharp
+using SparkNET.DotEnv;
+
+// Load
+Env.Load();
+```
