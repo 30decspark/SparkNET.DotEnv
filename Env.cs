c9b784c9ed05a10e69f3a0686e94fac0109ece90
30decspark <30decspark@gmail.com>
2025-03-28 13:15:26 +0700

    version 1.0.0

--- Env.cs
+++ Env.cs
@@ -5,34 +5,37 @@
         private static Dictionary<string, string> _data = [];
 
         public static void Load()
-        { 
+        {
             string path = ".env";
             if (!File.Exists(path))
             {
-                throw new Exception("The .env file does not exist!");
+                throw new Exception("The .env file not found!");
             }
 
             _data = [];
             var lines = File.ReadAllLines(path);
-            foreach (string line in lines)
+            foreach (var item in lines)
             {
-                if (string.IsNullOrWhiteSpace(line.Trim()) || line.Trim().StartsWith('#'))
-                {
-                    continue;
-                }
+                string line = item.Trim();
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith('#')) continue;
 
                 var kvp = line.Split('=', 2);
-                if (kvp.Length == 2)
-                {
-                    var key = kvp[0].Trim();
-                    var value = kvp[1].Trim();
+                if (kvp.Length != 2) continue;
+                
+                var key = kvp[0].Trim();
+                var value = kvp[1].Trim();
 
-                    if (!string.IsNullOrWhiteSpace(key) && !string.IsNullOrWhiteSpace(value))
-                    {
-                        _data[key] = value;
-                    }
-                }
+                if (string.IsNullOrWhiteSpace(key)) continue;
+                if (string.IsNullOrWhiteSpace(value)) continue;
+                _data[key] = value;
             }
+        }
+
+        public static T Get<T>(string key, T fallback = default!)
+        {
+            if (!_data.TryGetValue(key, out string? value)) return fallback;
+            try { return (T)Convert.ChangeType(value, typeof(T)); }
+            catch { return fallback; }
         }
     }
 }
